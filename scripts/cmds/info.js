1a4033f5d7ec6dcc14d10a5d39850fe8fdbd388b
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-09-16 00:38:38 +0100

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,23 +1,58 @@
 const fs = require('fs');
+const axios = require('axios');
 const moment = require('moment-timezone');
 const NepaliDate = require('nepali-date');
 const fast = require('fast-speedtest-api');
+const crypto = require('crypto');
+
+const AUTHOR = "Raphael ilom";
+const AUTHOR_HASH = crypto.createHash('sha256').update(AUTHOR).digest('hex');
+
+function verifyAuthor(name) {
+  return crypto.createHash('sha256').update(name).digest('hex') === AUTHOR_HASH;
+}
+
+async function getGokuImage() {
+  const fallbackImages = [
+    'https://i.imgur.com/1234567.jpg',
+    'https://i.imgur.com/2345678.jpg',
+    'https://i.imgur.com/3456789.jpg'
+  ];
+  try {
+    const response = await axios.get('https://api.jikan.moe/v4/characters/246/pictures', { timeout: 5000 });
+    const images = response.data.data;
+    return images[Math.floor(Math.random() * images.length)].jpg.image_url;
+  } catch (error) {
+    console.error('Error fetching Goku image:', error);
+    return fallbackImages[Math.floor(Math.random() * fallbackImages.length)];
+  }
+}
+
+async function getCryptoPrice(coin) {
+  try {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`, { timeout: 5000 });
+    return response.data[coin].usd;
+  } catch (error) {
+    console.error(`Error fetching ${coin} price:`, error);
+    return 'N/A';
+  }
+}
 
 module.exports = {
   config: {
     name: "info",
     aliases: ['info', 'owner'],
-    version: "1.3",
-    author: "AceGun",
+    version: "2.1",
+    author: AUTHOR,
     countDown: 5,
     role: 0,
     shortDescription: {
       vi: "",
-      en: "Sends information about the bot and admin along with an image."
+      en: "Sends information about the bot and admin along with a Goku image."
     },
     longDescription: {
       vi: "",
-      en: "Sends information about the bot and admin along with an image."
+      en: "Sends information about the bot and admin along with a Goku image."
     },
     category: "utility",
     guide: {
@@ -27,43 +62,52 @@ module.exports = {
   },
 
   onStart: async function ({ message, api, event, usersData, threadsData }) {
-    const allUsers = await usersData.getAll();
-    const allThreads = await threadsData.getAll();
-    const speedTest = new fast({
-        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
-        verbose: false,
-        timeout: 10000,
-        https: true,
-        urlCount: 5,
-        bufferSize: 8,
-        unit: fast.UNITS.Mbps
-      });
-    const result = await speedTest.getSpeed();
-    const botName = global.GoatBot.config.nickNameBot;
-    const botPrefix = global.GoatBot.config.prefix;
-    const authorName = global.GoatBot.config.authorName;
-    const authorFB = global.GoatBot.config.authorFB;
-    const authorInsta = "https://www.instagram.com/isaiahraphael606?igsh=MjQwMjNqejN1cWc4";
-    const authorEmail = global.GoatBot.config.authorEmail;
-    const authorGithub = "https://github.com/Isaiah-ilom";
-    const status = "𝙎𝙞𝙣𝙜𝙡𝙚";
+    if (!verifyAuthor(this.config.author)) {
+      return message.reply("Unauthorized modification detected. Command aborted.");
+    }
+
     const timeStart = Date.now();
 
-    const urls = JSON.parse(fs.readFileSync('scripts/cmds/xnil/info.json'));
-    const link = urls[Math.floor(Math.random() * urls.length)];
+    try {
+      const [allUsers, allThreads, speedTestResult, gokuImageUrl] = await Promise.all([
+        usersData.getAll(),
+        threadsData.getAll(),
+        new fast({
+          token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+          verbose: false,
+          timeout: 10000,
+          https: true,
+          urlCount: 5,
+          bufferSize: 8,
+          unit: fast.UNITS.Mbps
+        }).getSpeed(),
+        getGokuImage()
+      ]);
 
-    // Get current date and time in Asia/Kathmandu timezone
-    const now = moment().tz('Asia/Dhaka');
-    const date = now.format('MMMM Do YYYY');
-    const time = now.format('h:mm:ss A');
+      const botName = global.GoatBot.config.nickNameBot;
+      const botPrefix = global.GoatBot.config.prefix;
+      const authorName = global.GoatBot.config.authorName;
+      const authorFB = global.GoatBot.config.authorFB;
+      const authorInsta = "https://www.instagram.com/isaiahraphael606?igsh=MjQwMjNqejN1cWc4";
+      const authorEmail = global.GoatBot.config.authorEmail;
+      const authorGithub = "https://github.com/Isaiah-ilom";
+      const status = "𝙎𝙞𝙣𝙜𝙡𝙚";
 
-    // Calculate bot uptime
-    const uptime = process.uptime();
-    const uptimeString = formatUptime(uptime);
+      const now = moment().tz('Asia/Dhaka');
+      const date = now.format('MMMM Do YYYY');
+      const time = now.format('h:mm:ss A');
 
-    const ping = Date.now() - timeStart;
+      const uptime = process.uptime();
+      const uptimeString = formatUptime(uptime);
 
-    const replyMessage = `===「 Bot & Owner Info 」===
+      const ping = Date.now() - timeStart;
+
+      const [btcPrice, ethPrice] = await Promise.all([
+        getCryptoPrice('bitcoin'),
+        getCryptoPrice('ethereum')
+      ]);
+
+      const replyMessage = `===「 Bot & Owner Info 」===
 ❏ Bot Name: ${botName}
 ❏ Bot Prefix: ${botPrefix}
 ❏ Author Name: ${authorName}
@@ -77,14 +121,21 @@ module.exports = {
 ❏ Total Users: ${allUsers.length}
 ❏ Time: ${time}
 ❏ Bot Running: ${uptimeString}
-❏ Bot's Speed: ${result} MBPS
+❏ Bot's Speed: ${speedTestResult.toFixed(2)} MBPS
+❏ Ping: ${ping}ms
+❏ Bitcoin Price: $${btcPrice}
+❏ Ethereum Price: $${ethPrice}
 =====================`;
 
-    const attachment = await global.utils.getStreamFromURL(link);
-    message.reply({
-      body: replyMessage,
-      attachment
-    });
+      const attachment = await global.utils.getStreamFromURL(gokuImageUrl);
+      message.reply({
+        body: replyMessage,
+        attachment
+      });
+    } catch (error) {
+      console.error('Error in info command:', error);
+      message.reply("An error occurred while fetching information. Please try again later.");
+    }
   },
 
   onChat: async function({ event, message, getLang }) {
@@ -107,4 +158,4 @@ function formatUptime(uptime) {
   if (seconds > 0) uptimeString.push(`${seconds}sec`);
 
   return uptimeString.join(" ");
-}
+    }
